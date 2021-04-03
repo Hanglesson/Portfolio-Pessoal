3eea74a1d879193fa8f0103a31e1902972266d66
Hanglesson Fontines <72349939+Hanglesson@users.noreply.github.com>
2021-04-03 01:48:05 -0500

    :up: Seção Portfólio
    Implementação da galeria de projetos com filtro e exibição por imagens

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// ----------------Minhas Funções -----------------------
+function MudarTab() {}
 // ----------------Seção Sobre - TABS -----------------------
 (() => {
     const aboutSection = document.querySelector(".about-section"),
@@ -8,16 +10,180 @@
         if (event.target.classList.contains("tab-item") &&
             !event.target.classList.contains("active")) {
             const target = event.target.getAttribute("data-target")
-                // desativa a tab ativa
+
+            // desativa a tab ativa
             tabsContainer.querySelector(".active").classList.remove("outer-shadow", "active")
-                /* Ativa a nova tab */
+
+            /* Ativa a nova tab */
             event.target.classList.add("active", "outer-shadow")
-                /* Desativando a conteudo da tab*/
-            console.log(aboutSection.querySelector(".tab-content.active"))
+
+            /* Desativando a conteudo da tab*/
             aboutSection.querySelector(".tab-content.active").classList.remove("active")
-            console.log(aboutSection.querySelector(target))
-                // Ativando o novo conteúdo
+
+            // Ativando o novo conteúdo
             aboutSection.querySelector(target).classList.add("active")
         }
     })
-})()
+})();
+
+function bodyScrollkingToggle() {
+    document.body.classList.toggle("stop-scrolling")
+}
+
+// ----------------Seção Portfolio - Filtros e Popup -----------------------
+(() => {
+    const filterContainer = document.querySelector(".portfolio-filter"),
+        portfolioItemsContainer = document.querySelector(".portfolio-itens"),
+        portfolioItems = document.querySelectorAll(".portfolio-item"),
+        popup = document.querySelector(".portfolio-popup"),
+        prevBtn = popup.querySelector(".pp-prev"),
+        nextBtn = popup.querySelector(".pp-next"),
+        closeBtn = popup.querySelector(".pp-close"),
+        projectDetailsContainer = popup.querySelector(".pp-details"),
+        projectDetailsBtn = popup.querySelector(".pp-project-details-btn")
+
+    let itemIndex, slideIndex, screenshots
+
+    //filtro
+    filterContainer.addEventListener("click", (event) => {
+        if (event.target.classList.contains("filter-item") &&
+            !event.target.classList.contains("active")) {
+
+            //Desativando filtro ativo
+            filterContainer.querySelector(".active").classList.remove("outer-shadow", "active")
+
+            //Ativando novo Filtro
+            event.target.classList.add("active", "outer-shadow")
+            const target = event.target.getAttribute("data-target")
+
+            portfolioItems.forEach((item) => {
+                if (target === item.getAttribute("data-category") || target === "all") {
+                    item.classList.remove("hide")
+                    item.classList.add("show")
+                } else {
+                    item.classList.remove("show")
+                    item.classList.add("hide")
+                }
+            })
+        }
+    })
+
+    portfolioItemsContainer.addEventListener("click", (event) => {
+        if (event.target.closest(".portfolio-item-inner")) {
+            const portfolioItem = event.target.closest(".portfolio-item-inner").parentElement;
+
+            //Pega o index do item
+            itemIndex = Array.from(portfolioItem.parentElement.children).indexOf(portfolioItem);
+
+            screenshots = portfolioItems[itemIndex].querySelector(".portfolio-item-img img").getAttribute("data-screenshots");
+
+            // converte screenshots em um array
+            screenshots = screenshots.split(",")
+
+            // Verifica se o projeto tem mais de 1 foto
+            if (screenshots.length === 1) {
+                prevBtn.style.display = "none"
+                nextBtn.style.display = "none"
+            } else {
+                prevBtn.style.display = "block"
+                nextBtn.style.display = "block"
+            }
+
+            slideIndex = 0
+            popupToggle()
+            popupSlideshow()
+            popupDetails()
+        }
+    })
+    closeBtn.addEventListener("click", () => {
+        popupToggle()
+        if (projectDetailsContainer.classList.contains("active")) {
+            popupDetailsToggle()
+        }
+    })
+
+    function popupToggle() {
+        popup.classList.toggle("open")
+        bodyScrollkingToggle()
+    }
+
+    function popupSlideshow() {
+        const imgSrc = screenshots[slideIndex]
+        const popupImg = popup.querySelector(".pp-img")
+
+        // Ativando preload até que a imagem seja carregada
+        popup.querySelector(".pp-loader").classList.add("active")
+
+        // Popup recebe a imagem do projeto clicado dinamicamente
+        popupImg.src = imgSrc
+
+        popupImg.onload = () => {
+            // Desativa o preloader
+            popup.querySelector(".pp-loader").classList.remove("active")
+        }
+
+        // Modifica o contador dinamicamente
+        popup.querySelector(".pp-counter").innerHTML =
+            `${slideIndex + 1} de ${screenshots.length}`
+    }
+
+    // Próximo Slide
+    nextBtn.addEventListener("click", () => {
+        if (slideIndex === screenshots.length - 1) {
+            slideIndex = 0
+        } else {
+            slideIndex++
+        }
+        popupSlideshow()
+    })
+
+    // Anterior Slide
+    prevBtn.addEventListener("click", () => {
+        if (slideIndex === 0) {
+            slideIndex = screenshots.length - 1
+        } else {
+            slideIndex--
+        }
+        popupSlideshow()
+    })
+
+    //Pegar dados dos projetos e colocar na popup
+    function popupDetails() {
+        // Verifica se o projeto tem detalhes
+        if (!portfolioItems[itemIndex].querySelector(".portfolio-item-details")) {
+            projectDetailsBtn.style.display = "none"
+            return
+        } else {
+            projectDetailsBtn.style.display = "block"
+            const details = portfolioItems[itemIndex].querySelector(".portfolio-item-details").innerHTML
+            const title = portfolioItems[itemIndex].querySelector(".portfolio-item-title").innerHTML
+            const category = portfolioItems[itemIndex].getAttribute("data-category")
+
+            popup.querySelector(".pp-project-details").innerHTML = details
+            popup.querySelector(".pp-title h2").innerHTML = title
+            popup.querySelector(".pp-project-category").innerHTML = category.split("-").join(" ")
+        }
+
+    }
+
+    projectDetailsBtn.addEventListener("click", () => {
+        popupDetailsToggle()
+    })
+
+    function popupDetailsToggle() {
+        if (projectDetailsContainer.classList.contains("active")) {
+            projectDetailsContainer.classList.remove("active")
+            projectDetailsContainer.style.maxHeight = 0 + "px"
+
+            projectDetailsBtn.querySelector("i").classList.remove("fa-minus")
+            projectDetailsBtn.querySelector("i").classList.add("fa-plus")
+        } else {
+            projectDetailsBtn.querySelector("i").classList.remove("fa-plus")
+            projectDetailsBtn.querySelector("i").classList.add("fa-minus")
+
+            projectDetailsContainer.classList.add("active")
+            projectDetailsContainer.style.maxHeight = projectDetailsContainer.scrollHeight + "px"
+            popup.scrollTo(0, projectDetailsContainer.offsetTop)
+        }
+    }
+})();
